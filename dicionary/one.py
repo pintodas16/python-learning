41716acd98c3b3419c63306efa66f374146fc6a0
pintodas <pintodas.lu@gmail.com>
2025-06-18 02:56:02 +0600

    solving some dictionary related problem

---
+++ dicionary/one.py
@@ -0,0 +1,21 @@
+# Group Words by Length
+
+# Group a list of words into a dictionary based on their length.
+
+# Input: ['apple', 'dog', 'car', 'banana']
+
+# Output: {3: ['dog', 'car'], 5: ['apple'], 6: ['banana']}
+
+
+arr = ['apple', 'dog', 'car', 'banana'];
+arr = sorted(arr,key=len)
+print(arr)
+output={}
+
+for item in arr:
+    length = len(item)
+    if length in output:
+        output[length].append(item)
+    else:
+        output[length]=[item]
+print(output)
